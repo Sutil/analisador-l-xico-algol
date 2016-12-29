51021841ca495c5ed3ee73f8a8b3753608a2a9f5
Eduardo Sutil <sutil.edu@gmail.com>
2016-12-29 16:18:10 -0200

    specificator, brackets, and declarators

--- lexico/definidorToken.cpp
+++ lexico/definidorToken.cpp
@@ -9,15 +9,34 @@
 
 using namespace std;
 
-vector<string> TIPOS = {"int", "char"};
+vector<string> BRACKETS = {"begin", "end", "(", ")", "[", "]", "`", "'"};
+vector<string> TIPOS = {"own", "boolean", "integer","real", "array", "switch", "procedure"};
 vector<string> SIMBOLOS = {":=", "+"};
 vector<string> RELATIONAL_OPERATORS = {">", ">=", "==", "<", "<="};
+vector<string> SPECIFICATORS = {"string", "label", "value"};
 
 string NUM = "NUM";
-string TIPO = "TIPO";
+string TIPO = "declarator";
 string ID = "ID";
 string SIMBOLO = "SIMBOLO";
 string RELATIONAL_OPERATOR = "RELATIONAL_OPERATOR";
+string SPECIFICATOR = "specificator";
+
+bool isBracket(string valor){
+    for(int i = 0; i < BRACKETS.size(); i++)
+        if(BRACKETS[i].compare(valor) == 0)
+            return true;
+
+    return false;
+}
+
+bool isSpecificator(string valor){
+    for(int i = 0; i < SPECIFICATORS.size(); i++)
+        if(SPECIFICATORS[i].compare(valor) == 0)
+            return true;
+
+    return false;
+}
 
 bool isTipo(string valor){
     for(int i = 0; i < TIPOS.size(); i++)
@@ -58,8 +77,14 @@ string getTipoToken(string valor){
     if(isNumero(valor))
         return NUM;
 
+    if(isBracket(valor))
+        return "bracket";
+
     if(isTipo(valor))
         return TIPO;
+
+    if(isSpecificator(valor))
+        return SPECIFICATOR;
 
     if(isOperadorRelacional(valor))
         return RELATIONAL_OPERATOR;
--- tests/lexico/definidorTokenTest.cpp
+++ tests/lexico/definidorTokenTest.cpp
@@ -26,7 +26,37 @@ TEST(DefinidorToken, valorSemTipoDefinido){
 }
 
 TEST(DefinidorToken, valorTipoVariavel){
-    ASSERT_EQ("TIPO", getTipoToken("int"));
+    ASSERT_EQ("declarator", getTipoToken("integer"));
+}
+
+TEST(DefinidorToken, ownDeclarator){
+    ASSERT_EQ("declarator", getTipoToken("own"));
+}
+
+TEST(DefinidorToken, boolenaDeclarator){
+    ASSERT_EQ("declarator", getTipoToken("boolean"));
+}
+
+TEST(DefinidorToken, realDeclarator){
+    ASSERT_EQ("declarator", getTipoToken("real"));
+}
+
+TEST(DefinidorToken, arrayDeclarator){
+    ASSERT_EQ("declarator", getTipoToken("array"));
+}
+
+TEST(DefinidorToken, switchDeclarator){
+    ASSERT_EQ("declarator", getTipoToken("switch"));
+}
+
+TEST(DefinidorToken, procedureDeclarator){
+    ASSERT_EQ("declarator", getTipoToken("procedure"));
+}
+
+TEST(DefinidorToken, specificator){
+    ASSERT_EQ("specificator", getTipoToken("string"));
+    ASSERT_EQ("specificator", getTipoToken("label"));
+    ASSERT_EQ("specificator", getTipoToken("value"));
 }
 
 TEST(DefinidorToken, identificador){
@@ -39,6 +69,17 @@ TEST(DefinidorToken, simboloDeAtribuicao){
 
 TEST(DefinidorToken, simboloDeMais){
     ASSERT_EQ("SIMBOLO", getTipoToken("+"));
+}
+
+TEST(DefinidorToken, bracket){
+    ASSERT_EQ("bracket", getTipoToken("("));
+    ASSERT_EQ("bracket", getTipoToken(")"));
+    ASSERT_EQ("bracket", getTipoToken("["));
+    ASSERT_EQ("bracket", getTipoToken("]"));
+    ASSERT_EQ("bracket", getTipoToken("`"));
+    ASSERT_EQ("bracket", getTipoToken("'"));
+    ASSERT_EQ("bracket", getTipoToken("begin"));
+    ASSERT_EQ("bracket", getTipoToken("end"));
 }
 
 TEST(DefinidorToken, operadorRelacional){
