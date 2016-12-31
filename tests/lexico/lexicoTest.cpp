28410a4d146b70df6654e56f60170fa13aff8363
Eduardo Sutil <sutil.edu@gmail.com>
2016-12-31 15:26:28 -0200

    Analisador léxido lendo um programa de multiplicação de matrizes como este
    procedure multiMatriz(a, b, n, m)
        value n, m; array a, b, r; integer n, m;
    begin
        integer i, j, k, somatorio, produto;
        i := k := j = 0;
    
        for i := 1 step 1 until n do
            for q := 1 step 1 until m do
            begin
                somatorio := 0;
                for k:= 1 step 1 until m
                    begin
                        produto := a[i,k] * b[k, j];
                        somatorio := somatorio + produto;
                    end
                r[i, j] := somatorio;
            end
    
    end multiMatriz
    
    Mas ainda  precisamos ajustar o sintático.

--- tests/lexico/lexicoTest.cpp
+++ tests/lexico/lexicoTest.cpp
@@ -19,6 +19,14 @@ void assertToken(string valor, string nome, int linha, int coluna, Token* token)
     ASSERT_EQ(coluna, token->coluna);
 }
 
+void assertProximoToken(string valor, string nome, int linha, int coluna){
+    Token * token = getToken();
+    ASSERT_EQ(valor, token->valor);
+    ASSERT_EQ(nome, token->nome);
+    ASSERT_EQ(linha, token->linha);
+    ASSERT_EQ(coluna, token->coluna);
+}
+
 TEST(Lexico, tipoVariavelValido){
     FILE * f = criaArquivo("integer");
     inicializaAnalizadorLexico(f);
@@ -126,3 +134,92 @@ end multiMatriz
  */
 
 //criar um teste para fazer toda a análse deste programa.
+
+TEST(Lexico, programaCompleto){
+    FILE * f = criaArquivo("begin\n"
+                                   "procedure multiMatriz(a, b, n, m)\n"
+                                   "    value n, m; array a, b, r; integer n, m;\n"
+                                   "begin\n"
+                                   "    integer i, j, k, somatorio, produto;\n"
+                                   "    i := k := j := 0;\n"
+                                   "\n"
+                                   "    for i := 1 step 1 until n do\n"
+                                   "        for q := 1 step 1 until m do\n"
+                                   "        begin\n"
+                                   "            somatorio := 0;\n"
+                                   "            for k:= 1 step 1 until m\n"
+                                   "                begin\n"
+                                   "                    produto := a[i,k] * b[k, j];\n"
+                                   "                    somatorio := somatorio + produto;\n"
+                                   "                end\n"
+                                   "            r[i, j] := somatorio;\n"
+                                   "        end\n"
+                                   "\n"
+                                   "\n"
+                                   "end multiMatriz\n"
+                                   "end");
+    inicializaAnalizadorLexico(f);
+
+    assertProximoToken("begin", "bracket", 1, 1);
+    assertProximoToken("procedure", "declarator", 2, 1);
+    assertProximoToken("multiMatriz", "identifier", 2, 11);
+    assertProximoToken("(", "bracket", 2, 22);
+    assertProximoToken("a", "identifier", 2, 23);
+    assertProximoToken(",", "separator", 2, 24);
+    assertProximoToken("b", "identifier", 2, 26);
+    assertProximoToken(",", "separator", 2, 27);
+    assertProximoToken("n", "identifier", 2, 29);
+    assertProximoToken(",", "separator", 2, 30);
+    assertProximoToken("m", "identifier", 2, 32);
+    assertProximoToken(")", "bracket", 2, 33);
+    assertProximoToken("value", "specificator", 3, 5);
+    assertProximoToken("n", "identifier", 3, 11);
+    assertProximoToken(",", "separator", 3, 12);
+    assertProximoToken("m", "identifier", 3, 14);
+    assertProximoToken(";", "separator", 3, 15);
+    assertProximoToken("array", "declarator", 3, 17);
+    assertProximoToken("a", "identifier", 3, 23);
+    assertProximoToken(",", "separator", 3, 24);
+    assertProximoToken("b", "identifier", 3, 26);
+    assertProximoToken(",", "separator", 3, 27);
+    assertProximoToken("r", "identifier", 3, 29);
+    assertProximoToken(";", "separator", 3, 30);
+    assertProximoToken("integer", "declarator", 3, 32);
+    assertProximoToken("n", "identifier", 3, 40);
+    assertProximoToken(",", "separator", 3, 41);
+    assertProximoToken("m", "identifier", 3, 43);
+    assertProximoToken(";", "separator", 3, 44);
+    assertProximoToken("begin", "bracket", 4, 1);
+    assertProximoToken("integer", "declarator", 5, 5);
+    assertProximoToken("i", "identifier", 5, 13);
+    assertProximoToken(",", "separator", 5, 14);
+    assertProximoToken("j", "identifier", 5, 16);
+    assertProximoToken(",", "separator", 5, 17);
+    assertProximoToken("k", "identifier", 5, 19);
+    assertProximoToken(",", "separator", 5, 20);
+    assertProximoToken("somatorio", "identifier", 5, 22);
+    assertProximoToken(",", "separator", 5, 31);
+    assertProximoToken("produto", "identifier", 5, 33);
+    assertProximoToken(";", "separator", 5, 40);
+    assertProximoToken("i", "identifier", 6, 5);
+    assertProximoToken(":=", "separator", 6, 7);
+    assertProximoToken("k", "identifier", 6, 10);
+    assertProximoToken(":=", "separator", 6, 12);
+    assertProximoToken("j", "identifier", 6, 15);
+    assertProximoToken(":=", "separator", 6, 17);
+    assertProximoToken("0", "NUM", 6, 20);
+    assertProximoToken(";", "separator", 6, 21);
+    assertProximoToken("for", "sequential operator", 8, 5);
+    assertProximoToken("i", "identifier", 8, 9);
+    assertProximoToken(":=", "separator", 8, 11);
+    assertProximoToken("1", "NUM", 8, 14);
+    assertProximoToken("step", "separator", 8, 16);
+    assertProximoToken("1", "NUM", 8, 21);
+    assertProximoToken("until", "separator", 8, 23);
+    assertProximoToken("n", "identifier", 8, 29);
+    assertProximoToken("do", "sequential operator", 8, 31);
+
+
+    fclose(f);
+    remove("test.txt");
+}
