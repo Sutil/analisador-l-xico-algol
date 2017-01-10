fd563e9b9ad371f2955515ceb01f192f496bd6b1
Lucas <martiolilucas@gmail.com>
2017-01-10 21:09:10 -0200

    Iniciando implementação da tabela de variáveis e funções

---
+++ table/util.h
@@ -0,0 +1,33 @@
+//
+// Created by Lucas on 1/10/2017.
+//
+
+#ifndef PROJETOU_UTIL_H
+#define PROJETOU_UTIL_H
+
+#include <assert.h>
+#include <stdio.h>
+
+typedef char *string;
+typedef char bool;
+
+#define TRUE 1
+#define FALSE 0
+
+
+void *checked_malloc(int);
+string String(char *);
+
+typedef struct U_boolList_ *U_boolList;
+struct U_boolList_ {
+    bool head;
+    U_boolList tail;
+};
+U_boolList U_BoolList(bool head, U_boolList tail);
+int max(int a, int b);
+
+char *remove_ext (char* mystr, char dot, char sep) ;
+
+void indent(FILE *out, int d) ;
+
+#endif //PROJETOU_UTIL_H
