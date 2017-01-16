ec74f2a158725815b8033ab2e4df4961592a1481
Eduardo Sutil <sutil.edu@gmail.com>
2017-01-16 20:51:56 -0200

    Escopos sendo empilhados corretamente na análise semantica

--- semantico/AnalisadorSemantico.cpp
+++ semantico/AnalisadorSemantico.cpp
@@ -17,6 +17,18 @@ bool isDeclaracaoVariavel(No * no){
     return no->nome.compare("type declaration") == 0;
 }
 
+bool isAtribuicaoDeVariavel(No * no){
+    return no->nome.compare("assignment statement") == 0;
+}
+
+bool isFimDeEscopo(No * no){
+    return isInicioDeEscopo(no);
+}
+
+void verifica(No * no){
+    //cout << "verifica atribuição" << endl;
+}
+
 void varrerVariaveis(No * no, string tipo){
     if(no->isTerminal() && no->nome.compare(",") != 0) {
         cout << tipo << " " << no->nome << endl;
@@ -24,67 +36,49 @@ void varrerVariaveis(No * no, string tipo){
         Tabela * tab = pilha.top();
         tab->addRegistro(r);
 
+
         return;
     }
 
     for(No * filho : no->filhos)
         varrerVariaveis(filho, tipo);
-
 }
 
-void varrerBloco(No * bloco){
 
+void percurso(No * no){
 
-    for(No * filho : bloco->filhos){
-
-        if(isDeclaracaoVariavel(filho)){
-            No* localOrOwnType = filho->filhos[0];
-            No * tipo = localOrOwnType->filhos[0];
-            No * tipoVariavel = tipo->filhos[0];
-            No * typeList = filho->filhos[1];
-
-            varrerVariaveis(typeList, tipoVariavel->nome);
-
-            continue;
-        }
-
-        if(isInicioDeEscopo(filho)) {
-            cout << endl << filho->nome << " " << filho->profundiade << endl;
-            Tabela * topo = pilha.top();
-            Tabela * t = topo->copy(filho->profundiade);
-            pilha.push(t);
-
-            varrerBloco(filho);
-
-            cout << "fim do escopo" << filho->profundiade << endl << endl;
-            pilha.pop();
-        }
-
-        varrerBloco(filho);
-    }
-}
-
-void percorreArvore(No * no){
-
-
-    if(isInicioDeEscopo(no)){
-        cout << no->nome << " " << no->profundiade << endl;
-        Tabela * t = new Tabela(no->profundiade);
+    if(isInicioDeEscopo(no)) {
+        cout << "inicio escopo " << no->profundiade << endl;
+        Tabela *t = new Tabela(no->profundiade);
         pilha.push(t);
-
-        varrerBloco(no);
-        cout << "fim do escopo" << no->profundiade << endl << endl;
-        pilha.pop();
     }
 
-    for(No * n : no->filhos)
-        percorreArvore(n);
+    if(isDeclaracaoVariavel(no)) {
+        No* localOrOwnType = no->filhos[0];
+        No * tipo = localOrOwnType->filhos[0];
+        No * tipoVariavel = tipo->filhos[0];
+        No * typeList = no->filhos[1];
+        varrerVariaveis(typeList, tipoVariavel->nome);
+    }
 
+    if(isAtribuicaoDeVariavel(no)){
+        verifica(no);
+    }
 
+    for(No * filho : no->filhos){
+        percurso(filho);
+
+        if(isFimDeEscopo(no)){
+            pilha.pop();
+            cout << "fim de escopo " << no->profundiade << endl << endl;
+        }
+    }
 }
+
+
 
 
 bool aliseSemantica(No * raiz){
-    percorreArvore(raiz);
+    percurso(raiz);
 }
 
